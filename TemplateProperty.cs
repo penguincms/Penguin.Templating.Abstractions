c822eaa1d5c78dd8cb4ef68f04d943417646cdc0
anloffredo <anloffredo@massageenvy.com>
2020-05-14 19:36:37 -0700

    Auto Commit

--- TemplateProperty.cs
+++ TemplateProperty.cs
@@ -5,9 +5,27 @@ namespace Penguin.Templating.Abstractions
 {
     public class TemplateProperty : ITemplateProperty
     {
-        public List<ITemplateProperty> Children { get; set; }
+        /// <summary>
+        /// A collection of child properties
+        /// </summary>
+        public List<ITemplateProperty> Children { get; } = new List<ITemplateProperty>();
+
+        /// <summary>
+        /// Adds a child template property
+        /// </summary>
+        /// <param name="toAdd">The child to add</param>
+        public void AddChild(ITemplateProperty toAdd)
+        {
+            Children.Add(toAdd);
+        }
+
+
         IEnumerable<ITemplateProperty> ITemplateProperty.Children => this.Children;
+        /// <summary>
+        /// The name to display in any editor used to alter this object
+        /// </summary>
         public string DisplayName { get; set; }
+
         public string MacroBody { get; set; }
     }
 }
